3e9ba2a648d8b0cee96a2fceb9d6644c5b4d50c2
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2022-12-12 12:47:38 +0700

    nambahin comment di function putri

--- functionPutri.py
+++ functionPutri.py
@@ -2,19 +2,26 @@ from functionAlfa import restrictedInput, tablelize
 from functionFajar import validateUserSelection
 
 def batalPesan(listPembelian):
+  # Tampilin list pembelian yang udah ada
   tablelize(listPembelian, 'rounded_grid')
+  
+  # panggil fungsi cancelPesanan
   cancelPesanan(listPembelian)
   
+  # Tanya user, mau batalin pesanan lagi engga?
   batalLagiGa = restrictedInput('Ingin batalkan pesanan lagi? Y/N: ', 'Input yang tersedia hanya Y/N', ['y', 'n'], str)
   
   while batalLagiGa == 'y':
-    cancelPesanan(listPembelian)
+    # kalo user input y, bakal ngulangin process diatas
     tablelize(listPembelian, 'rounded_grid')
+    cancelPesanan(listPembelian)
     batalLagiGa = restrictedInput('Ingin batalkan pesanan lagi? Y/N: ', 'Input yang tersedia hanya Y/N', ['y', 'n'], str)
     
 def cancelPesanan(listPembelian):
+  # User input kode menu mana yang mau di hapus
   noBatal = input('Masukan kode menu yang ingin dibatalkan: ')
   
+  # Validasi kode yang dimasukin user harus ada di list pembelian
   validateUser = validateUserSelection(listPembelian, noBatal.upper())
   
   while validateUser != True:
@@ -24,10 +31,16 @@ def cancelPesanan(listPembelian):
     validateUser = validateUserSelection(listPembelian, noBatal.upper())
   
   for i in range(len(listPembelian)):
+    print(listPembelian[i])
+    # Cek kalo kodenya sama kaya yang di masukin user
     if listPembelian[i]['Kode Menu'] == noBatal.upper():
+      
+      # Cek is random nya True apa engga
       if listPembelian[i]['isRandom'] == True:
+        # Kalo isRandomnya True, gabisa dihapus
         print('Pesanan yang dari randomate tidak bisa dibatalkan !!!')
       else :
+        # Kalo false, Bakal dihapus
         del listPembelian[i]
         print('Pesanan dibatalkan')
         break
